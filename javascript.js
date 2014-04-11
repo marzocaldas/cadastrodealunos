41949478134231f64bf36a70240954a289b84607
Marzo Caldas <marzocaldas@gmail.com>
2014-04-11 16:01:50 -0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,259 @@
+/* Arquivo JavaScript: validações, funções específicas e AJAX */
+
+function validacao(){
+	/*1)Validações dos dos itens nomes e 'Comente um pouco sobre você'. 
+	Ambas devem obrigar que o usuário digite pelo menos DUAS PALAVRAS*/ 
+	var twoWordsRegex = new RegExp(/(^[A-Za-z]{2,}\s[A-Za-z-']{3,}$)/);//usando uma expressão regular para validação do nome e sobrenome (duas palavras)
+	if(!twoWordsRegex.test(document.frmhobbie.txt_nome.value)){
+		alert("Por favor, preencha o nome e sobrenome.");
+		document.frmhobbie.txt_nome.focus();
+			return false;
+	}
+	
+	
+	if((document.frmhobbie.txa_comentario.value < 10 || document.frmhobbie.txa_comentario.value > 1000)){ //validação minimo de duas palavras(usei um minimo de 10 e um maximo de 1000 caracteres)
+		alert("Por favor, comente um pouco sobre você, use pelo menos duas palavras.");
+		document.frmhobbie.txa_comentario.focus();
+			return false;
+	}
+	
+	/*2)	Valdando o item Endereço, o qual deve conter pelo menos DUAS LETRAS.
+	O item Sexo deve validar se o usuário marcou o campo*/
+	if(document.frmhobbie.txt_endereco.value.length < 2){
+		alert("Por favor, preencha o compo endereço com pelo menos duas letras.");
+		document.frmhobbie.txt_endereco.focus();
+			return false;
+	}
+	if (!document.frmhobbie.rdo_sexo[0].checked && !document.frmhobbie.rdo_sexo[1].checked) {
+			alert('Por favor, elecione o Sexo');
+			return false;
+		}
+		
+	/*3)	Validando o item Hobbies, o usuário deve marcar no mínimo UM HOBBIE. O campo Descreva é obrigatório
+	e deve conter pelo menos DUAS LETRAS enquanto o item Outros estiver marcado */
+	if(!document.getElementById('chk_hobbies_0').checked && !document.getElementById('chk_hobbies_1').checked && !document.getElementById('chk_hobbies_2').checked &&
+	!document.getElementById('chk_hobbies_3').checked && !document.getElementById('chk_hobbies_4').checked && !document.getElementById('chk_hobbies_5').checked && 
+	!document.getElementById('chk_hobbies_6').checked && !document.getElementById('chk_hobbies_7').checked && !document.getElementById('chk_hobbies_8').checked){
+
+		 alert('Por favor, selecione algum Hobbie.');
+		return false;
+		}
+	if(document.getElementById('chk_hobbies_8').checked && document.frmhobbie.txt_outros.value.length < 2){//se o checkbox outros está marcado obriga descrição no campo de texto ao lado
+	
+		alert("Por Favor, descreva seu hobbie.");
+		document.frmhobbie.txt_outros.focus();
+		return false;
+		}
+		
+		/*4)	Validando o item Idade, somente deve aceitar a digitação de números. 
+		O número digitado deve ser maior que 2 e inferior a 150 */
+		if(document.frmhobbie.txt_idade.value == ""){ //verifica se capo está vazio
+		alert("Por favor, informe a idade.")
+		document.frmhobbie.txt_idade.focus();
+		return false;
+		}
+		if(isNaN(document.frmhobbie.txt_idade.value) || document.frmhobbie.txt_idade.value > 150){ //verifica se o campo é número e menor que 150
+		alert("Digite apenas números. Valor máximo 150.")
+		document.frmhobbie.txt_idade.focus();
+		return false;
+		}
+}
+
+/*
+AJAX
+*/
+
+function loadJSON(url) {
+var http;
+if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	http = new XMLHttpRequest();
+} else { // code for IE6, IE5
+	http = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+http.onreadystatechange = function() {
+	if (http.readyState == 4 && http.status == 200) {
+			eval( "result = " + http.responseText );
+			
+	var table = document.getElementById('tableAtividades'); // Tabela de atividades
+	var row = null; 
+	var cell = null; 
+	
+	if (table.rows.length < 2) { 
+		row = table.insertRow(0);  
+	} else {
+		row = table.rows[ table.rows.length - 2]; 
+	}
+	
+	if (row.cells.length < 2) {  
+		cell = row.insertCell(row.cells.length - 1); 
+	} else {
+		row = table.insertRow(table.rows.length - 1); 
+		cell = row.insertCell(row.cells.length - 1);  
+	}
+	
+	
+	var checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+	checkbox.name = "chk_atividades[]";
+	checkbox.value = result.id;
+	checkbox.id = "chk_atividades" + result.id;
+
+	var label = document.createElement('label')
+	label.htmlFor = "chk_atividades" + result.id;
+	label.appendChild(document.createTextNode( result.atividade ));
+	
+	cell.appendChild(checkbox); 
+	cell.appendChild(label); 
+			
+}
+}
+http.open("GET", url, true); 
+http.send(); 
+}
+
+function loadHobbiesTabelaAJAX(url) {
+var http;
+if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	http = new XMLHttpRequest();
+} else { // code for IE6, IE5
+	http = new ActiveXObject("Microsoft.XMLHTTP");
+}
+http.onreadystatechange = function() {
+	if (http.readyState == 4 && http.status == 200) {
+			eval( "result = " + http.responseText );
+			for (i=0; i < result.length; i++) {
+	var table = document.getElementById('tableAtividades'); // Tabela de atividades
+	var row = null; // declara var
+	var cell = null; // delara var
+	
+	if (table.rows.length < 2) { 
+		row = table.insertRow(0);  
+	} else {
+		row = table.rows[ table.rows.length - 2]; 
+	}
+	
+	if (row.cells.length < 2) {  
+		cell = row.insertCell(row.cells.length - 1); 
+	} else {
+		row = table.insertRow(table.rows.length - 1); 
+		cell = row.insertCell(row.cells.length - 1);  
+	}
+	
+	var checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+	checkbox.name = "chk_atividades[]";
+	checkbox.value = result[i].idatividade;
+	checkbox.id = "chk_atividades" + result[i].idatividade;
+
+	var label = document.createElement('label')
+	label.htmlFor = "chk_atividades" + result[i].idatividade;
+	 label.appendChild(document.createTextNode( result[i].nomeatividade ));
+	
+	cell.appendChild(checkbox); 
+	cell.appendChild(label); 
+	}
+			
+}
+}
+http.open("GET", url, true); 
+http.send(); 
+}
+
+
+
+
+function addHobbie()
+{
+
+var atividade = prompt("Adicione uma atividade no campo abaixo");
+if (atividade != null)
+{
+  loadJSON("insereatividade.php?atividade=" + atividade);
+}
+}
+
+
+/*Função que valida data e mostra a idade*/
+function showAge()
+{
+
+//inicio validação data
+
+  var dataCampo = document.getElementById("txt_idade").value;
+  var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  
+  if(dataCampo.match(dateformat))
+  {
+
+  var opera1 = dataCampo.split('/');
+  var opera2 = dataCampo.split('-');
+  lopera1 = opera1.length;
+  lopera2 = opera2.length;
+
+  if (lopera1>1)
+  {
+  var pdate = dataCampo.split('/');
+  }
+  else if (lopera2>1)
+  {
+  var pdate = dataCampo.split('-');
+  }
+  var dd = parseInt(pdate[0]);
+  var mm  = parseInt(pdate[1]);
+  var yy = parseInt(pdate[2]);
+  // Create list of days of a month [assume there is no leap year by default]
+  var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+  if (mm==1 || mm>2)
+  {
+  if (dd>ListofDays[mm-1])
+  {
+  alert('Data inválida. Por favor, digite: DD/MM/AAA');
+  return false;
+  }
+  }
+  if (mm==2)
+  {
+  var lyear = false;
+  if ( (!(yy % 4) && yy % 100) || !(yy % 400)) 
+  {
+  lyear = true;
+  }
+  if ((lyear==false) && (dd>=29))
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  if ((lyear==true) && (dd>29))
+  {
+  alert('Data inválida. Por favor, digite: DD/MM/AAA');
+  return false;
+  }
+  }
+  }
+  else
+  {
+  alert('Data inválida. Por favor, digite: DD/MM/AAA');
+  dataCampo.focus();
+  return false;
+  }
+//fim validação data
+
+//mostra idade no label ao lado do campo de texto
+var hoje = new Date();
+var idade = hoje.getFullYear() - yy;
+
+if(mm>(hoje.getMonth()+1))
+{
+idade--;
+} 
+else if(mm=(hoje.getMonth()+1) && dd>hoje.getDate()){
+idade--;
+}
+document.getElementById("age").textContent = "Idade: "+idade+" anos";
+document.getElementById("hid_age").value = idade;
+}
+
+
+
+
