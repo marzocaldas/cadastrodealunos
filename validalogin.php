87615dee7be21d7156d51d1c71a0bf75abff211c
Marzo Caldas <marzocaldas@gmail.com>
2014-04-14 17:42:26 -0300

    Create validalogin.php
    This code validates log in and sends to a specific page.

---
+++ validalogin.php
@@ -0,0 +1,35 @@
+<?php
+
+
+if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']))) {
+	header("Location: index.php"); exit;
+}
+
+
+mysql_connect('localhost', 'root', '') or trigger_error(mysql_error());
+
+mysql_select_db('cadastro_alunos') or trigger_error(mysql_error());
+
+$usuario = mysql_real_escape_string($_POST['usuario']);
+$senha = mysql_real_escape_string($_POST['senha']);
+
+
+$sql = "SELECT `id`, `nome`, `nivel` FROM `usuarios` WHERE (`usuario` = '". $usuario ."') AND (`senha` = '". sha1($senha) ."') AND (`ativo` = 1) LIMIT 1";
+$query = mysql_query($sql);
+if (mysql_num_rows($query) != 1) {
+
+	echo "Login inválido!"; exit;
+} else {
+
+	$resultado = mysql_fetch_assoc($query);
+
+	if (!isset($_SESSION)) session_start();
+
+	$_SESSION['UsuarioID'] = $resultado['id'];
+	$_SESSION['UsuarioNome'] = $resultado['nome'];
+	$_SESSION['UsuarioNivel'] = $resultado['nivel'];
+
+	header("Location: restrito.php"); exit;
+}
+
+?>
