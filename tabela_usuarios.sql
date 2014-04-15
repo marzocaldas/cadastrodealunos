7ebdb727ee1aec197f7f3f936cccdb9ee89b9640
Marzo Caldas <marzocaldas@gmail.com>
2014-04-15 16:51:58 -0300

    Create tabela_usuarios.sql

---
+++ tabela_usuarios.sql
@@ -0,0 +1,28 @@
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+-- Banco de Dados: `cadastro_alunos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `usuario` varchar(25) NOT NULL,
+  `senha` varchar(40) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `nivel` int(1) unsigned NOT NULL DEFAULT '1' COMMENT 'Normal (1) e Administrador (2)',
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  `cadastro` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`),
+  KEY `niveis` (`nivel`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
