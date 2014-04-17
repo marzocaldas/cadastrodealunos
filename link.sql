2547a3b64cadd2f51ce9711dd817da384e0de209
Marzo Caldas <marzocaldas@gmail.com>
2014-04-17 17:59:34 -0300

    Create link.sql

---
+++ link.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS link (
+  idlink int(11) NOT NULL auto_increment,
+  link varchar(1000) NOT NULL,
+  PRIMARY KEY  (idlink)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
