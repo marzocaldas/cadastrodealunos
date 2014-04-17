9e47c74194e676dea60b043533ff1463e6be4782
Marzo Caldas <marzocaldas@gmail.com>
2014-04-17 17:54:11 -0300

    Update restrito.php

--- restrito.php
+++ restrito.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
 	header("Location: login.php"); exit;
 }
 
+
 ?>
 <html>
 <a href="logout.php">logout</a>
@@ -24,7 +25,30 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
 <a href="https://drive.google.com/uc?export=download&confirm=Gi0j&id=0B6AlAoNcQOW_UmloR1lFRmk0OTQ">The Simpsons S02E01</a>
 <br/>
 <a href="https://docs.google.com/uc?export=download&confirm=qY9K&id=0B6AlAoNcQOW_UWRQcFhNS2lOeE0">Earthquakes</a>
+<br/>
+<?php
+$idusuariosessao = $_SESSION['UsuarioID'];
 
+$con=mysqli_connect("localhost","root","","cadastro_alunos");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$result = mysqli_query($con,"select l.* from link l inner join aluno_link al on l.idlink = al.idlink where id = $idusuariosessao");
+
+while($row = mysqli_fetch_array($result))
+  {
+
+  echo $row['link'];
+
+
+echo "<br>";
+  }
+
+mysqli_close($con);
+?>
 
 
 <html/>
